7370661b779383e14e02077cad8e513ca29150e6
hussienail <63175073+hussienail@users.noreply.github.com>
2020-12-02 18:36:18 +0300

    Create ASIM12.js

---
+++ ASIM12.js
@@ -0,0 +1,45 @@
+try{
+														$(`<a id="عاصم" href=""><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a><img id="asim70"src="/sico/1606663771123.jpg
+"style="width:100%"><a id="asim27"target="_blank"type="a"rel="nofollow"href="https://www.dookmobile.com"class="label-primary">Programming by Dookmobile</a></div></div><center>`).insertBefore(".nav-tabs"),$(".nav-tabs").addClass("fl").css("width","100%"),
+
+$(".nav-tabs").addClass("fl").css("width","100%")
+$(`<center><div id="asim1">
+<a id="asim7"rel="dofollow" title="القوانين" href="#"style="border-radius: 0 11px 0 0; border-right: 2px solid#f7d322;"class="fl mini  fa fa-bullhorn btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="الاشتراكات" href="#"style="border-right: 1px solid#fff;"class="fl mini  fa fa-star-half-o btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="راديو" href="#"style="border-right: 1px solid#fff;"class="fl mini  fa fa-volume-up btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="زخرفة اسماء" href="#"style="border-right: 1px solid#fff;"class="fl mini  fa fa-fire btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="اتصل بنا" href="#"style="border-right: 1px solid#fff;"class="fl mini fa fa-paper-plane btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="قوانين السوابر" href="#"style="border-right: 1px solid#fff;"class="fl mini fa fa-exclamation-triangle btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="تطبيق الشات" href="#"style="border-right: 1px solid#fff;"class="fl mini fa fa-android btn btn-primary" target="_blank"></a>
+<a id="asim7"rel="dofollow" title="اختصارات الشات" href="#"style="border-radius: 13px 0 0 0; border-left: 3px solid#f7d322; border-right: 1px solid#ffffff;"class="fl mini fa fa-tencent-weibo btn btn-primary" target="_blank"></a>
+</div></div>
+<center><div><marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;أهلا وسهلا بكم في شات <font color="#165916" class="asim12">  نـجـمـه الــحـب  </font> ● · ‫أفضل سوبر لهذا الأسبوع · ● <font class="asim11" color="#727A54">&nbsp; وائــل &nbsp;</font> ● · ‫أفضل زائر لهذا الأسبوع · ● <font class="asim11" color="#ff9900">&nbsp;الــربــيــعــي &nbsp;</font> ● · ‫أفضل زائرة لهذا الأسبوع · ● <font class="asim11" color="#ff00ff">&nbsp; ســومه &nbsp;</font> ● · مبدع الحائط لهذا الأسبوع · ● <font class="asim11" color="#5CAD9A">&nbsp; مـدردش&nbsp;</font> ● · مبدعة الحائط لهذا الأسبوع · ● <font class="asim11" color="#800000">&nbsp; بـنـيـن الـدراجــي&nbsp;</font> ● · ‫أفضل تواجد لهذا الأسبوع · ● <font class="asim11" color="#417570">&nbsp; غـزل ال شــمــر&nbsp;</font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;
+</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break'),
+
+$(`<link rel="stylesheet" href="https://raw.githack.com/hussienail/Hos2/master/as13m.css"></style>`).insertBefore("body");
+													}
+														catch (e){
+															console.log(e)
+														}
+												
+					
+
+vchat=false;
+$(".fa-gear").text("  ").css("width","");
+
+$(".fa-commenting-o").text("   ").css("width","");
+
+$(".fa-users").text("  ").css("width","");
+
+
+
+$("a.fa.fa-user-plus").text('تسجيل');
+$("a.fa.fa-user ").text('دخـول');
+
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="https://awtar-iq.com/sico/1604666696923.png
+" width="70%" style="">`)
+
+$("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star-half-o'></span>لوحة تحكم شات نجمه الحب ")
+
+$ ( ` <center> <div style =" width: 100٪؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample"> Happy birthdayBanu      َ</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+
